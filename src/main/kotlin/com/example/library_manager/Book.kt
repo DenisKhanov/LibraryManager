d36d6c36fb7eb739f7b01ea641080d02d795ecc8
denis <denis_hanov@hotmail.com>
2025-07-21 14:53:10 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/library_manager/Book.kt
@@ -0,0 +1,4 @@
+package com.example.library_manager
+
+class Book {
+}
---
+++ src/main/kotlin/com/example/library_manager/LibraryManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.library_manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibraryManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<LibraryManagerApplication>(*args)
+}
